a1476218dd92d2e2edf8d18188d70d72068456bd
Inderpal Singh <singhinther@gmail.com>
2017-04-13 21:11:55 +0200

    sendMessageOnCurrentlyOpenedChat

--- listen.js
+++ listen.js
@@ -1,1 +1,7 @@
-console.log("Hello")
+function sendMessageOnCurrentlyOpenedChat(message) {
+  var input = document.querySelector('.block-compose .input');
+  input.innerHTML = message;
+  input.dispatchEvent(new Event('input', {bubbles: true}));
+  var button = document.querySelector('.block-compose button.icon-send');
+  button.click();
+}
