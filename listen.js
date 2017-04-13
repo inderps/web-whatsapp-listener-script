10ad2e8f84892cf249670ff999d17165aed7e170
Inderpal Singh <singhinther@gmail.com>
2017-04-13 22:35:27 +0200

    formatting

--- listen.js
+++ listen.js
@@ -1,24 +1,27 @@
 function sendMessageOnCurrentChat(message) {
-  var input = document.querySelector('.block-compose .input');
-  input.innerHTML = message;
-  input.dispatchEvent(new Event('input', {bubbles: true}));
-  var button = document.querySelector('.block-compose button.icon-send');
-  button.click();
+    var input = document.querySelector('.block-compose .input');
+    input.innerHTML = message;
+    input.dispatchEvent(new Event('input', {
+        bubbles: true
+    }));
+    var button = document.querySelector('.block-compose button.icon-send');
+    button.click();
 }
 
 function subscribeToMessagesOnCurrentChat(subscribeCallback) {
-  var observer = new MutationObserver(function(mutations) {
-   mutations.forEach(function(mutation) { 
-     for (var i = 0; i < mutation.addedNodes.length; i++)
-       var msgNode = mutation.addedNodes[i];
-       if (msgNode) {
-         var textNode = msgNode.querySelector(".msg .message .emojitext.selectable-text")
-         if (textNode) {
-           subscribeCallback(textNode.textContent);
-         }
-       }
-   })
-  });
-
-  observer.observe($(".message-list"), { childList: true });
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            for (var i = 0; i < mutation.addedNodes.length; i++)
+                var msgNode = mutation.addedNodes[i];
+            if (msgNode) {
+                var textNode = msgNode.querySelector(".msg .message .emojitext.selectable-text")
+                if (textNode) {
+                    subscribeCallback(textNode.textContent);
+                }
+            }
+        })
+    });
+    observer.observe($(".message-list"), {
+        childList: true
+    });
 }
